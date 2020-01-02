5e0069282fd40514d14ce95cb1dfd5cc21726406
AmrH <AmrH@DEV-AMR>
2020-01-02 16:52:43 +0200

    Add project files.

---
+++ SearchBarRTL/SearchBarRTL/MainArabic.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SearchBarRTL
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainArabic : ContentPage
+    {
+        public MainArabic()
+        {
+            InitializeComponent();
+        }
+
+        private void GoToEnglish(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new MainPage());
+        }
+    }
+    
+}
